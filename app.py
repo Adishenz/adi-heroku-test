fc94cc8daff246b666d4dc8d0ecd65f5a069fab9
Adishenz <41304894+Adishenz@users.noreply.github.com>
2021-07-26 13:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-PAGE_CONFIG = {"page_title":"Text2Handwriting","page_icon":"images/page_logo.png"}
+PAGE_CONFIG = {"page_title":"Text2Handwriting","page_icon":"https://img.icons8.com/wired/64/000000/bookmark.png"}
 st.set_page_config(**PAGE_CONFIG)
 
 
